0af0e3e8273a05f6d29ae13c30793819bc33c7c0
programmervarunshridhar <46110005+programmervarunshridhar@users.noreply.github.com>
2025-03-03 11:21:05 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,23 @@
 
+use master;
+
+--Drop and recreate the 'YTDWB' database
+IF exists (select 1 from sys.databases where name = 'YTDWB')
+begin
+	alter database YTDWB set single_user with rollback immediate;
+	drop database YTDWB;
+end;
+go
+
+create database YTDWB;
+go
+
+use YTDWB;
+go
+
+-- Create schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
